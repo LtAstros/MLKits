61d423af1f8baaf6d04029e446f7c90c4eff469a
Costin Smilovici <c.smilovici@csmilovic-ltm.internal.salesforce.com>
2019-07-22 13:55:23 -0700

    Updated code to match possition in tutorial

--- regressions/index.js
+++ regressions/index.js
@@ -6,15 +6,19 @@ const LinearRegression = require('./linear-regression.js')
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
     shuffle: true,
     splitTest: 50,
-    dataColumns: ['horsepower'],
+    dataColumns: ['horsepower', 'weight', 'displacement'],
     labelColumns: ['mpg'],
 });
 
 const regression = new LinearRegression(features, labels, {
-    learningRate: .00001,
-    iterations: 100000
-});
-
-regression.train();
-
-console.log('Updated M is:', regression.m, 'Updated B is:', regression.b)
+    learningRate: 0.1,
+    iterations: 3,
+    batchSize: 10
+  });
+  
+  regression.train();
+  const r2 = regression.test(testFeatures, testLabels);
+  
+  console.log('R2 is', r2);
+  
+  regression.predict([[120, 2, 380]]).print();
